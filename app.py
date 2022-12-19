f354b774b2489a9c6fdf30e5eb5173bea9a37800
KhoaDD6 <dinhdangkhoa0201@gmail.com>
2022-12-19 22:18:36 +0700

    Fix lint Using an f-string that does not have any interpolated variables (f-string-without-interpolation)

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def checkHealth():
-    html = f"<h2>Welcome to my page! My name is KhoaDD6</h2><h6>App version is v1.0.0</h6>"
+    version = "v1.0.0"
+    html = f"<h2>Welcome to my page! My name is KhoaDD6</h2><h6>App version is ${version}</h6>"
     return html.format(format)
 
 if __name__ == "__main__":
