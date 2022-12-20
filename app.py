d2b2fb51c575d66ff58c5e7ff31d7476694d8bf9
KhoaDD6 <dinhdangkhoa0201@gmail.com>
2022-12-20 22:05:59 +0700

    deploy version v2.0.0

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def checkHealth():
-    version = "v1.0.0"
+    version = "v2.0.0"
     html = f"<h2>Welcome to my page! My name is KhoaDD6</h2><h6>App version is ${version}</h6>"
     return html.format(format)
 
